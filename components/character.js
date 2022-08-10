3e181c65e0007e393830ab8f2597f0431e054874
adhemukhlis <adhemukhlis@gmail.com>
2022-08-11 01:33:35 +0700

    split component

---
+++ components/character.js
@@ -0,0 +1,9 @@
+const CharacterComponent = () => {
+  return (
+    <div className="character" facing="down" walking="true">
+      <div className="shadow pixel-art"></div>
+      <div className="character_spritesheet pixel-art"></div>
+    </div>
+  );
+};
+export default CharacterComponent;
---
+++ components/corner.js
@@ -0,0 +1,11 @@
+const CornerComponent = () => {
+  return (
+    <>
+      <div className="corner_topleft"></div>
+      <div className="corner_topright"></div>
+      <div className="corner_bottomleft"></div>
+      <div className="corner_bottomright"></div>
+    </>
+  );
+};
+export default CornerComponent;
